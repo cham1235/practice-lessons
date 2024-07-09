7f4afa6850fca8cc9a0282cae28ff73a8ea91aef
cham1235 <hana171819@gmail.com>
2024-07-09 21:26:03 +0900

    Improving User Experience with Loading Skeletons

---
+++ src/components/GameCardSekeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSekeleton = () => {
+  return (
+    <>
+      <Card width="250px">
+        <Skeleton height="200px" />
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default GameCardSekeleton;
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -24,18 +24,25 @@ export interface Platform {
 const useGame = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-  const controller = new AbortController();
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
     apiClient
-      .get("/games")
-      .then((res) => setGames(res.data.results))
+      .get<FetchGamesRespose>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
+
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGame;
