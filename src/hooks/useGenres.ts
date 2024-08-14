15b00bcde3d77bf21acf11973d36bfd9f11a9cbd
cham1235 <hana171819@gmail.com>
2024-08-14 21:02:58 +0900

    Shipping Static Data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,13 @@
-import useData from "./useData";
+import genres from "../data/genres";
+// import useData from "./useData";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+// from API
+// const useGenres = () => useData<Genre>("/genres");
+const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
